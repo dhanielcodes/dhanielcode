a884c0b135cf2eb4611459cda30d8dced12b5d16
Adekoya Daniel <adekoyadaniel53@gmail.com>
2021-01-26 16:11:55 +0100

    ++

---
+++ src/view/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="footer">
+    <footer>
+      <h1>Get int touch !</h1>
+      <div class="line"></div>
+      <a class="mail" href="mailto:adekoyadaniel53@gmail.com" target="_blank"
+        >adekoyadaniel53@gmail.com</a
+      >
+      <div class="socials">
+        <a href="http://">
+          <img src="../assets/twitter.svg" alt="" />
+        </a>
+        <a href="http://">
+          <img src="../assets/linkedin.svg" alt="" />
+        </a>
+        <a href="http://">
+          <img src="../assets/github.svg" alt="" />
+        </a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  padding: 10px;
+}
+h1 {
+  font-weight: 300;
+  font-size: 40px;
+}
+.line {
+  width: 500px;
+  height: 1px;
+  background: white;
+  margin: 30px 0;
+}
+.socials {
+  margin-top: 30px;
+}
+.socials img {
+  margin: 0 10px;
+}
+@media (max-width: 580px) {
+  .line {
+    width: 100%;
+  }
+  .socials img {
+  }
+}
+</style>
